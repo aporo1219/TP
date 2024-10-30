a6e52036c4fd54adea5733984391484c00c1122c
kouta <h1202419006@hiratagakuen.onmicrosoft.com>
2024-10-30 11:41:49 +0900

    新しいソースツリー

---
+++ TeamProject/Assets/sakaguchi/Script/Mouse.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Mouse : MonoBehaviour
+{
+    private int number;
+    private int number1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;//カーソル非表示
+        Cursor.lockState = CursorLockMode.Locked;//カーソル機能なし
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         
+        if (SceneManager.GetActiveScene().name == "GameOver")
+        {
+            Cursor.visible = true;//カーソル表示
+            Cursor.lockState = CursorLockMode.None;//カーソル機能あり
+            ClickGO(number);
+        }
+        if (SceneManager.GetActiveScene().name == "GameOver")
+        {
+            Cursor.visible = true;//カーソル表示
+            Cursor.lockState = CursorLockMode.None;//カーソル機能あり
+            ClickGC(number1);
+        }
+    }
+
+    public void ClickGO(int N)
+    {
+       switch(N)
+        {
+            case 1:
+                SceneManager.LoadScene("Unity");
+                break;
+            case 2:
+                SceneManager.LoadScene("Title");
+                break;
+            default:
+                break;
+        }
+    }
+    public void ClickGC(int N1)
+    {
+        switch (N1)
+        {
+            case 1:
+                SceneManager.LoadScene("StageChoicee");
+                break;
+            case 2:
+                SceneManager.LoadScene("Title");
+                break;
+            default:
+                break;
+        }
+    }
+}
+   
+
+   

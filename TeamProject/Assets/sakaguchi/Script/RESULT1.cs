3473f9f8ac343e10733344bce0e601251b90977f
kouta <h1202419006@hiratagakuen.onmicrosoft.com>
2024-11-18 16:21:23 +0900

    ゲームクリア画面

--- TeamProject/Assets/sakaguchi/Script/RESULT1.cs
+++ TeamProject/Assets/sakaguchi/Script/RESULT1.cs
@@ -6,17 +6,50 @@ using UnityEngine.SceneManagement;
 
 public class RESULT1 : MonoBehaviour
 {
+    //スイーツの表示(マカロン）
+    public GameObject objectToShowM;
+    private bool isDisplayedM;
+    //(パンケーキ)
+    public GameObject objectToShowPancake;
+    private bool isDisplayedPancake;
+    //(パフェ）
+    public GameObject objectToShowParfait;
+    private bool isDisplayedParfait;
+    //(ケーキ)
+    public GameObject objectToShowC;
+    private bool isDisplayedC;
+    //()
+    public GameObject objectToShow;
+    private bool isDisplayed;
     public Text calorieresult;//テキストの表示
     // Start is called before the first frame update
     void Start()
     {
-
+        //マカロン
+        isDisplayedM = false;
+        objectToShowM.SetActive(false);
+        //パンケーキ
+        isDisplayedPancake = false;
+        objectToShowPancake.SetActive(false);
+        //パフェ
+        isDisplayedParfait = false;
+        objectToShowParfait.SetActive(false);
+        //ケーキ
+        isDisplayedC = false;
+        objectToShowC.SetActive(false);
+        //
+        isDisplayed = false;
+        objectToShow.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         calorieresult.text = "カロリーは" + CalorieTEXT.CalorieTEXTInitival + "Kcal残ったよ。".ToString();//カロリーのテキスト文字をカロリーの初期値におく
+        if()
+        {
+            GameObject.
+        }
     }
 
 

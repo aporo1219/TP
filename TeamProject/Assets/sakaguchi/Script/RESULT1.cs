fcab5498709074327b7ace570e7f683fad804476
urata_ayase <h1202419003@hiratagakuen.onmicrosoft.com>
2024-11-19 11:44:09 +0900

    BGM、SEの追加

--- TeamProject/Assets/sakaguchi/Script/RESULT1.cs
+++ TeamProject/Assets/sakaguchi/Script/RESULT1.cs
@@ -8,83 +8,47 @@ public class RESULT1 : MonoBehaviour
 {
     //スイーツの表示(マカロン）
     public GameObject objectToShowM;
-   
+    private bool isDisplayedM;
     //(パンケーキ)
     public GameObject objectToShowPancake;
-    
+    private bool isDisplayedPancake;
     //(パフェ）
     public GameObject objectToShowParfait;
-    
+    private bool isDisplayedParfait;
     //(ケーキ)
     public GameObject objectToShowC;
-   
-    //(プリン)
-    public GameObject objectToShowPudding;
-  
-
+    private bool isDisplayedC;
+    //()
+    public GameObject objectToShow;
+    private bool isDisplayed;
     public Text calorieresult;//テキストの表示
     // Start is called before the first frame update
     void Start()
     {
         //マカロン
-        
-        objectToShowM.SetActive(true);
+        isDisplayedM = false;
+        objectToShowM.SetActive(false);
         //パンケーキ
-        
+        isDisplayedPancake = false;
         objectToShowPancake.SetActive(false);
         //パフェ
-        
+        isDisplayedParfait = false;
         objectToShowParfait.SetActive(false);
         //ケーキ
-        
+        isDisplayedC = false;
         objectToShowC.SetActive(false);
-        //プリン
-        
-        objectToShowPudding.SetActive(false);
+        //
+        isDisplayed = false;
+        objectToShow.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         calorieresult.text = "カロリーは" + CalorieTEXT.CalorieTEXTInitival + "Kcal残ったよ。".ToString();//カロリーのテキスト文字をカロリーの初期値におく
-        if(SceneManager.GetActiveScene().name == "stage2")
-        {
-            STAGE2GC();
-        }
-        if (SceneManager.GetActiveScene().name == "stage3")
-        {
-            STAGE3GC();
-        }
-        if (SceneManager.GetActiveScene().name == "stage4")
-        {
-            STAGE4GC();
-        }
-        if (SceneManager.GetActiveScene().name == "stage5")
-        {
-            STAGE5GC();
-        }
-
-    }
-    void STAGE2GC()//ステージ２をクリアしたらプリンの表示
-    {
-        objectToShowM.SetActive(false);
-        objectToShowPudding.SetActive(true);
-    }
-    void STAGE3GC()//ステージ3をクリアしたらパフェの表示
-    {
-        objectToShowM.SetActive(false);
-        objectToShowParfait.SetActive(true);
+       
+        
     }
 
-    void STAGE4GC()//ステージ4をクリアしたらパンケーキの表示
-    {
-        objectToShowM.SetActive(false);
-        objectToShowPancake.SetActive(true);
-    }
 
-    void STAGE5GC()//ステージ5をクリアしたらケーキの表示
-    {
-        objectToShowM.SetActive(false);
-        objectToShowC.SetActive(true);
-    }
 }

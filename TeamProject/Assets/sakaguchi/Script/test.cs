fdde0904a0672913d4ac2c8a9d9ad2bd67df9a04
kouta <h1202419006@hiratagakuen.onmicrosoft.com>
2024-11-22 11:15:49 +0900

    しゃがみアニメーションの改善

---
+++ TeamProject/Assets/sakaguchi/Script/test.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    //•à‚­
+    // private float STEP = 3.0f;
+    //‚µ‚á‚ª‚Þ
+    private Animator anim = null;
+    
+    
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        
+    }
+
+    void Update()
+    {
+        float horizontalKey = Input.GetAxis("Horizontal");
+
+        if (horizontalKey < 0)
+        {
+            anim.SetBool("crouch", true);
+           
+            
+        }
+       
+        else 
+        {
+           anim.SetBool("crouch", false);
+          
+        } 
+         
+        //‰EŒü‚«
+        // this.transform.position += new Vector3(STEP * Time.deltaTime, 0, 0);
+    }
+}
